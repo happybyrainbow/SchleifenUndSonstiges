c81c8de930083acd6270ae5e19c8516f9906e37c
happybyrainbow <maximturianskyi@gmail.com>
2023-04-18 22:24:21 +0200

    a

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,8 +1,10 @@
+import java.sql.SQLOutput;
+
 public class MainProgram {
 
     public static void main(String[] args) {
 
-        methode();
+        System.out.println(sumUp(1,7));
 
     }
 
@@ -16,5 +18,24 @@ public class MainProgram {
         }
     }
 
+    public static void printSomeNumbers(){
+        for(int i = 6; i <= 23; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void printNumbers(int start, int end){
+        for(int i = start; i <= end; i++){
+            System.out.println(i);
+        }
+    }
+
+    public static int sumUp(int start, int end) {
+        int a = 0;
+        for (int i = start; i <= end; i++) {
+            a += i;
+        }
+        return a;
+    }
 }
 
