a86edfb7d5adcb5a44ee7c5ba87d42df8eea6564
turimaks <maximturianskyi@gmail.com>
2023-04-18 10:42:38 +0200

    Initial commit

---
+++ src/MainProgram.java
@@ -0,0 +1,20 @@
+public class MainProgram {
+
+    public static void main(String[] args) {
+
+        methode();
+
+    }
+
+    public static void methode() {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Wert von i: "+i);
+            if (i == 3) {
+                System.out.println("Holdriho !");
+            }
+        }
+    }
+
+}
+
